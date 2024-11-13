63d1848283efbfef6f46c9086a2507b38940791d
Gaham <glebsemenov1mail.ru@gmail.com>
2024-11-13 22:22:57 +0300

    Update true.js

--- true.js
+++ true.js
@@ -1,6 +1,6 @@
 function TRUE(program,input){
   var stack=[];var output='';var temp=[];
   for(var p=0;p<program.length;p++){
-    if (program[p]=='"'){while(program[p+1]!=='"'){p++;temp.push(program[p])};};
+    if (program[p]=='"'){while(program[p+1]!=='"'){p++;temp.push(program[p]);return temp};};
   };
   return output};
