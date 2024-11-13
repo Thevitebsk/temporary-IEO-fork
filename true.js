b1aa4e56953cf48270bd9a11d95680c69f91d3e6
Gaham <glebsemenov1mail.ru@gmail.com>
2024-11-13 21:52:52 +0300

    Create true.js

---
+++ true.js
@@ -0,0 +1,9 @@
+function TRUE(program,input){
+  var stack=[];var output='';var temp=[];
+  for(var p=0;p<program.length;p++){
+    if (program[p]=='"'){p++
+      while(program[p+1]!='"'){output+=program[p]}
+      };
+  };
+  return output
+  };
