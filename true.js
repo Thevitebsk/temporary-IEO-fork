6d8a3e67442dc8881bf61b41b6bf67e9beb47083
Gaham <glebsemenov1mail.ru@gmail.com>
2024-11-13 22:27:13 +0300

    Delete true.js

--- true.js
+++
@@ -1,6 +0,0 @@
-function TRUE(program,input){
-  var stack=[];var output='';var temp=[];
-  for(var p=0;p<program.length;p++){
-    if (program[p]=='"'){while(program[p+1]!=='"'){temp.push(program[p]);p++};};
-  };
-  return output};
